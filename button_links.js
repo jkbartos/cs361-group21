4423f9ffd1c96e7766ae3dd952f2be69036a808f
cprumsey21 <51032749+cprumsey21@users.noreply.github.com>
2019-05-29 16:41:19 -0400

    Update button_links.js

--- button_links.js
+++ button_links.js
@@ -1,12 +1,14 @@
-/* This file is to be used to navigate pages if a hyperlink is not wanted
-Store this file in the Public directory and reference it in the context.jsscripts section of each js script */
-
 // The page where the user can add obstacles to the obstacle table when passing in obstacle type from a drop-down and entering latitude and longitude
 function gotoAddObstacles() {
     window.location = '/obstacles/add'
 }
 
 // The page where the user can test if the data base returns obstacles when passing in a a search radius and a starting latitude and longitude
+function gotoGetObstacles() {
+    window.location = '/obstacles/add'
+}
+
+// The page with a table that shows all obstacles when ariving from a different page or from add_obstacle, the table shows the data returned from getObstacles
 function gotoObstacles() {
     window.location = '/obstacles'
 }
