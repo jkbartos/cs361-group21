11fe32744ceeaf3dd34666d97abe5d7ba5b0bb74
cprumsey21 <51032749+cprumsey21@users.noreply.github.com>
2019-05-29 12:28:56 -0400

    Update user_story_10_functions.js

--- user_story_10_functions.js
+++ user_story_10_functions.js
@@ -1,3 +1,8 @@
+/* This file is to be used to by the user story 10 get obstacles section.
+Store this file in the Public directory and reference it in the context.jsscripts section of each story_10 page's js */
+
+// This function is called when the user clicks submit on the user_story_10 page.
+//The input fields must be named 'radius', 'deg_lat' and 'deg_long'
 function searchObstaclesByLocationAndRadius() {
     var p_rad = document.getElementById('radius').value
     var p_lat = document.getElementById('deg_lat').value
