5cd9dff8c54ea1c55c6422dd74c194b2e5853dbb
cprumsey21 <51032749+cprumsey21@users.noreply.github.com>
2019-05-29 17:21:35 -0400

    Update obstacles_functions.js

--- obstacles_functions.js
+++ obstacles_functions.js
@@ -1,8 +1,8 @@
 /* This file is to be used to by the user story 10 get obstacles section.
 Store this file in the Public directory and reference it in the context.jsscripts section of each obstacles js call definition */
 
-// This function is called when the user clicks submit on the user_story_10 page.
-//The input fields must be named 'radius', 'deg_lat' and 'deg_long'
+// This function is called when the user clicks submit on the get_obstacles page.
+//The input fields must have the IDs of 'radius', 'deg_lat' and 'deg_long'
 function searchObstaclesByLocationAndRadius() {
     var p_rad = document.getElementById('radius').value
     var p_lat = document.getElementById('deg_lat').value
@@ -10,24 +10,12 @@ function searchObstaclesByLocationAndRadius() {
     window.location = '/obstacles/search/results/?p_radius=' + encodeURI(p_rad) + '&p_deg_lat=' + encodeURI(p_lat) + '&p_deg_long=' + encodeURI(p_long)
 }
 
+// This function is called when the user clicks submit on the add_obstacles page.
+// The input fields must have the IDs of deg_lat, deg_lon, ob_type 
 function addObstacle() {
-
-    var checkboxes = document.getElementsByName("fuels");
-    var fuels = "";
-    for (var i = 0, n = checkboxes.length; i < n; i++) {
-        if (checkboxes[i].checked) {
-            if (fuels.length > 1) {
-                fuels += "," + checkboxes[i].value;
-            }
-            else {
-                fuels += checkboxes[i].value;
-            }
-        }
-    }
-
     var p_lat = document.getElementById('deg_lat').value
     var p_lon = document.getElementById('deg_lon').value
-    var p_ob_type = document.getElementById('veh_cat').value
+    var p_ob_type = document.getElementById('ob_type').value
 
     var others_deg_lat = document.getElementsByName('others_lat');
     var others_deg_lon = document.getElementsByName('others_lon');
