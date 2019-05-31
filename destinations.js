1a20a45e1e4ca166489fdbcabc24b8728486cdf8
cprumsey21 <51032749+cprumsey21@users.noreply.github.com>
2019-05-31 14:46:47 -0400

    Update destinations.js

--- destinations.js
+++ destinations.js
@@ -2,10 +2,38 @@ module.exports = function () {
     var express = require('express');
     var router = express.Router();
 
+    // Need to update this function with VehicleID query once a table is built with vehicle IDs
+    function getVehicleIDs(res, mysql, context, complete) {
+        mysql.pool.query("SELECT DISTINCT ot.obstacle_type FROM obstacle_types ot order by ot.obstacle_type;", function (error, results, fields) {
+            if (error) {
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.vehicleIDs = results;
+            complete();
+        });
+    }
+
+
+    router.get('/set/', function (req, res) {
+        var callbackCount = 0;
+        var context = {};
+        context.jsscripts = ["destination_functions.js", "button_links.js"];
+        var mysql = req.app.get('mysql');
+        getVehicleIDs(res, mysql, context, complete);
+        function complete() {
+            callbackCount++;
+            if (callbackCount >= 1) {
+                res.render('set_destination', context);
+            }
+        }
+    });
+
+
     router.get('/', function (req, res) {
         var callbackCount = 0;
         var context = {};
-        context.jsscripts = ["button_links.js"];
+        context.jsscripts = ["destination_functions.js", "button_links.js"];
         var mysql = req.app.get('mysql');
         res.render('destinations', context);
     });
