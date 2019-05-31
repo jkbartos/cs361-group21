1f9d08e924812293fc383d87add925988c6237f1
cprumsey21 <51032749+cprumsey21@users.noreply.github.com>
2019-05-31 14:50:18 -0400

    Update destinations.js

--- destinations.js
+++ destinations.js
@@ -3,6 +3,8 @@ module.exports = function () {
     var router = express.Router();
 
     // Need to update this function with VehicleID query once a table is built with vehicle IDs
+    // This will be used to provide data for a dropdown menu in the form so that the user can select
+    // valid vehicle IDs when setting the destination for a vehicle ID.
     function getVehicleIDs(res, mysql, context, complete) {
         mysql.pool.query("SELECT DISTINCT ot.obstacle_type FROM obstacle_types ot order by ot.obstacle_type;", function (error, results, fields) {
             if (error) {
@@ -13,6 +15,20 @@ module.exports = function () {
             complete();
         });
     }
+    
+    
+    // Need to update this function with destination update query once the destination and vehicle ID tables are built
+    // Query should take in the vehicle ID and destination as parameters and should update the destination of the 
+    // record for that vehicle ID.
+    function updateDestination(req, res, mysql, complete) {
+        mysql.pool.query("set @p_deg_lat = " + decodeURI(req.query.par_deg_lat) + "; set @p_deg_long = " + decodeURI(req.query.par_deg_long) + "; set @p_ob_type = " + mysql.pool.escape(decodeURI(req.query.par_ob_type)) + "; INSERT INTO obstacles(latitude,longitude,obstacle_type) values(@p_deg_lat, @p_deg_long, @p_ob_type);", function (error) {
+            if (error) {
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            complete();
+        });
+    }    
 
 
     router.get('/set/', function (req, res) {
