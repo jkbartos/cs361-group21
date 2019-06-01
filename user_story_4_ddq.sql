d5cb15f87f251f45599f9244244f33fa8f44eeba
cprumsey21 <51032749+cprumsey21@users.noreply.github.com>
2019-06-01 01:18:10 -0400

    Update user_story_4_ddq.sql

--- user_story_4_ddq.sql
+++ user_story_4_ddq.sql
@@ -2,7 +2,7 @@
 CREATE TABLE `destinations`(
 `vehicle_id`int(11) NOT NULL,
 `step_id`int(11) NOT NULL,
-`street_name`varchar(255),
+`instruction`varchar(255),
 `duration_min`int(11),
 `distance_mi`double(11,1),
 PRIMARY KEY (`vehicle_id`,`step_id`)
