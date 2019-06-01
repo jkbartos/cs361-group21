af4e3da89a6636446dbbd263dafd8948fad37d37
cprumsey21 <51032749+cprumsey21@users.noreply.github.com>
2019-06-01 01:19:53 -0400

    Update user_story_4_ddq.sql

--- user_story_4_ddq.sql
+++ user_story_4_ddq.sql
@@ -7,3 +7,23 @@ CREATE TABLE `destinations`(
 `distance_mi`double(11,1),
 PRIMARY KEY (`vehicle_id`,`step_id`)
 ) ENGINE =INNODB;
+
+
+-- BUILD stip_tags FUNCTION
+delimiter ||
+DROP FUNCTION IF EXISTS strip_tags||
+CREATE FUNCTION strip_tags( x longtext) RETURNS longtext
+LANGUAGE SQL NOT DETERMINISTIC READS SQL DATA
+BEGIN
+DECLARE sstart INT UNSIGNED;
+DECLARE ends INT UNSIGNED;
+SET sstart = LOCATE('/', x, 1);
+REPEAT
+SET ends = LOCATE('\', x, sstart);
+SET x = CONCAT(SUBSTRING( x, 1 ,sstart -1) ,SUBSTRING(x, ends +1 )) ;
+SET sstart = LOCATE('/', x, 1);
+UNTIL sstart < 1 END REPEAT;
+return x;
+END;
+||
+delimiter ;
