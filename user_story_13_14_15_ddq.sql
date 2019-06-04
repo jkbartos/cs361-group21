147aabf084dee3bf4f5ec9c93ad827b8cd151707
schmcory <36436049+schmcory@users.noreply.github.com>
2019-06-03 23:18:26 -0500

    Update user_story_13_14_15_ddq.sql

--- user_story_13_14_15_ddq.sql
+++ user_story_13_14_15_ddq.sql
@@ -3,7 +3,7 @@
 This file does not need to be included with the website files. */
 
 -- BUILD PARKING SPACE TABLE
-CREATE TABLE `parking_spaces`(
+CREATE TABLE `parking`(
 `parking_id`int(11) NOT NULL AUTO_INCREMENT,
 `status` bit DEFAULT 0 NOT NULL,
 `latitude`double(12,9) NOT NULL,
